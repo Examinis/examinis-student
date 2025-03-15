c4a0edff7c252d37a5870517b02d6eb6d879bc7d
Yago César <yagocdj@gmail.com>
2025-03-15 15:13:54 +0000

    feat: update user registration to permit nested contact attributes, enhance layout with sidebar navigation, and add logo

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,12 +48,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name,
+      contacts_attributes: [ :id, :contact_type, :value, :_destroy ] ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name,
+      contacts_attributes: [ :id, :contact_type, :value, :_destroy ] ])
   end
 
   # The path used after sign up.
