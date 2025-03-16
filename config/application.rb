bc2f5a8f64fb3141c875807a396d6c099fcefe40
Yago César <yagocdj@gmail.com>
2025-03-16 17:42:00 +0000

    feat: add Brazilian Portuguese locale support for error messages and devise translations

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module ExaminisStudent
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Locale configuration
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.available_locales = %i[en pt-BR]
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.fallbacks = [ :en ]
   end
 end
